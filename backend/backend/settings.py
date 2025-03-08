2bc46c852ac0bd135e79f56c2ba032bfb7779d6d
PedroJoseBastos <pjbb.bastos@gmail.com>
2025-03-07 21:44:03 -0400

    treno

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = 'api.CustomUser'
 
 # Application definition
 
@@ -62,6 +63,7 @@ CORS_ALLOW_HEADERS = [
     'Authorization'
 ]
 
+
 CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'backend.urls'
@@ -69,7 +71,7 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
